d8206e9b61cb5b8bb5e196b226a3afca590e5c4b
kaufon <kauanfvale@gmail.com>
2024-05-04 11:46:11 -0300

    🤓solve: 1188 1190

---
+++ src/iniciante/1188/Bee1188.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    char Escolha;
+    int escolha;
+    double rows = 12,cols = 12;
+    vector<vector<double>> matrix(rows,vector<double>(cols));
+
+    cin >>Escolha;
+    if (Escolha == 'S')
+    {
+        escolha = 0;
+    }
+    else{
+        escolha = 1;
+    }
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            scanf("%lf",&matrix[i][j]);
+        }
+        
+    }
+    float sum = 0;
+    int cont = 0;
+    for (int j = 1; j <= 10; ++j) {
+        sum += matrix[11][j];
+        cont++;
+    }
+
+    for (int j = 2; j <= 9; ++j) {
+        sum += matrix[10][j];
+        cont++;
+    }
+     for (int j = 3; j <= 8; ++j) {
+        sum += matrix[9][j];
+        cont++;
+    }
+    for (int j = 4; j <= 7; ++j) {
+        sum += matrix[8][j];
+        cont++;
+    }
+
+    for (int j = 5; j <=6 ; ++j)
+    {
+        sum += matrix[7][j];
+        cont++;
+    }
+    
+    
+    switch (escolha)
+    {
+    case 0:
+        printf("%.1f\n",sum);
+        break;
+    
+    case 1:
+        printf("%.1f\n",sum/cont);
+        break;
+    }
+    return 0;
+}
