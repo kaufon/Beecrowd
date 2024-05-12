4ea9e42ad9c5993ae2c924422dc8b2296cf72b0f
kaufon <kauanfvale@gmail.com>
2024-05-12 11:23:25 -0300

    🚧in progress: 1046

---
+++ src/iniciante/1046/Bee1046.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int inicio,fim,contador = 0;
+    scanf("%d %d",&inicio,&fim);
+    for (int i = inicio; i != fim; i++)
+    {
+    
+       if (i == 24)
+       {
+        i = 0;
+       }
+       contador++;
+       
+    }
+    if (inicio == fim)
+    {
+        contador = 24;
+    }
+    
+    printf("O JOGO DUROU %d HORA(S)\n",contador);
+    
+
+
+
+    return 0;
+}
