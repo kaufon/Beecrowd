c95251d51b0bf22b1a3ff81b2cd6237c24ea7244
kaufon <kauanfvale@gmail.com>
2024-05-12 00:12:01 -0300

    🤓solve: 1040

--- src/iniciante/1040/Bee1040.cpp
+++ src/iniciante/1040/Bee1040.cpp
@@ -3,9 +3,35 @@
 using namespace std;
 
 int main(){
-    
+    float n1,n2,n3,n4,exame,media = 0;
+    scanf("%f %f %f %f",&n1,&n2,&n3,&n4);
+    media = (n1*2 + n2*3 + n3*4 + n4*1) / 10;
+    printf("Media: %.1f\n",media);
 
-
-
+    if (media >= 7.0)
+    {
+        
+        cout << "Aluno aprovado." <<endl;
+    }
+    else if (media < 5)
+    {
+        
+        cout << "Aluno reprovado." <<endl;
+    }
+    else
+    {
+        cout << "Aluno em exame." <<endl;
+        scanf("%f",&exame);
+        printf("Nota do exame: %.1f\n",exame);
+        if ((media + exame) / 2 >= 5 )
+        {
+            cout << "Aluno aprovado." << endl;
+        }
+        else
+        {
+            cout << "Aluno reprovado.." <<endl;
+        }
+        printf("Media final: %.1f\n",(media + exame) / 2);
+    }
     return 0;
 }
