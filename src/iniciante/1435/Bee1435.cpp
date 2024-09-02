4c65084d11ee53a8d1cdc26114108d3b4fddc774
kaufon <kauanfvale@gmail.com>
2024-09-02 17:15:50 -0300

    🤓solve:1435

--- src/iniciante/1435/Bee1435.cpp
+++ src/iniciante/1435/Bee1435.cpp
@@ -3,16 +3,30 @@
 using namespace std;
 int main(int argc, char *argv[]) {
   int n = 1;
+  int x = 0;
   while (cin >> n && n != 0) {
     int rows = n, cols = n;
-    vector<vector<int>> matrix(rows, vector<int>(cols));
-    for (int i = 0; i < rows; i++) {
-      for (int j = 0; j < cols; j++) {
-        matrix[i][j] = abs(1);
-        if (j == 0) {
-          cout << setw(3) << matrix[i][j];
+    vector<vector<int>> matrix(rows+1, vector<int>(cols+1));
+    for (int rowForCount = 1; rowForCount <= rows; rowForCount++) {
+      for (int columnForCount = 1; columnForCount <= cols; columnForCount++) {
+        x = rowForCount;
+        if (columnForCount < x) {
+          x = columnForCount;
+        }
+        if (n - rowForCount + 1 < x) {
+          x = n - rowForCount + 1;
+          
+        }
+        if (n - columnForCount +1 < x) {
+          x = n - columnForCount + 1;
+          
+        }
+        matrix[rowForCount][columnForCount] = x;
+
+        if (columnForCount == 0) {
+          cout << setw(3) << matrix[rowForCount][columnForCount];
         } else {
-          cout << " " << setw(3) << matrix[i][j];
+          cout << " " << setw(3) << matrix[rowForCount][columnForCount];
         }
       }
       cout << endl;
