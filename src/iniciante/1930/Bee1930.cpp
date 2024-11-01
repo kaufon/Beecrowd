395434a41a9bc0d264236951a7b5a56511276e2d
kaufon <kauanfvale@gmail.com>
2024-10-31 21:49:40 -0300

    1930

---
+++ src/iniciante/1930/Bee1930.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+#include <cstdlib>
+using namespace std;
+int main(int argc, char *argv[]) {
+  int t1, t2, t3, t4, result;
+  cin >> t1 >> t2 >> t3 >> t4;
+  result = t1 - 1 + t2 - 1 + t3 - 1 + t4;
+  cout << result << endl;
+
+  return 0;
+}
