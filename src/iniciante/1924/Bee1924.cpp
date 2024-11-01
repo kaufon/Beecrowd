31f0f48a401e4ead25f9062f6bb9f7b5d16b6f0b
kaufon <kauanfvale@gmail.com>
2024-10-31 21:45:05 -0300

    1924

---
+++ src/iniciante/1924/Bee1924.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#include <cstdlib>
+using namespace std;
+int main(int argc, char *argv[]) {
+  int n;
+  cin >> n;
+  while (n--) {
+    int *f = new int;
+    cin >> *f; 
+    delete f;
+  };
+  cout << "Ciencia da Computacao" << endl;
+  return 0;
+}
