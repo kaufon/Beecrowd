a80a7bb82fd087f39f7ad54f398349340d6a08fd
kaufon <kauanfvale@gmail.com>
2024-11-06 21:23:45 -0300

    1984

---
+++ src/iniciante/1984/Bee1984.cpp
@@ -0,0 +1,11 @@
+#include <algorithm>
+#include <bits/stdc++.h>
+#include <string>
+using namespace std;
+int main(int argc, char *argv[]) {
+  string str;
+  cin >> str;
+  reverse(str.begin(), str.end());
+  cout << str << endl;
+  return 0;
+}
