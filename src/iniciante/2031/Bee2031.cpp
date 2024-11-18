e055aeb67ec1878062bc758722afc270b13b6400
kaufon <kauanfvale@gmail.com>
2024-11-18 20:04:55 -0300

    2031

--- src/iniciante/2031/Bee2031.cpp
+++ src/iniciante/2031/Bee2031.cpp
@@ -1,9 +1,6 @@
-
 #include <bits/stdc++.h>
 using namespace std;
 int main(int argc, char *argv[]) {
-  map<string, vector<string>> Game = {
-      {"pedra", {"ataque"}}, {"papel", {"pedra,ataque"}}, {"ataque", {}}};
   int n;
   cin >> n;
   while (n--) {
@@ -11,23 +8,19 @@ int main(int argc, char *argv[]) {
     cin >> player1 >> player2;
     if (player1 == player2) {
       if (player1 == "papel") {
-        cout << "Ambos Venceram" << endl;
-
+        cout << "Ambos venceram" << endl;
       } else if (player1 == "ataque") {
         cout << "Aniquilacao mutua" << endl;
-      }
-      else {
+      } else {
         cout << "Sem ganhador" << endl;
       }
-    } else if ((player1 == "ataque" ||
-                (find(Game[player1].begin(), Game[player1].end(), player2) ==
-                     Game[player1].end() &&
-                 player2 != "ataque"))) {
-      cout << "Jogador 1 venceu" << endl;
-    } else {
-      cout << "Jogador 2 venceu" << endl;
+    }else if(player1 == "ataque"){
+      cout << "Jogador 1 venceu" <<endl;
+    }else if(player1 == "pedra" && player2 == "papel"){
+      cout << "Jogador 1 venceu" <<endl;
+    }else{
+      cout << "Jogador 2 venceu" <<endl;
     }
   }
-
   return 0;
 }
