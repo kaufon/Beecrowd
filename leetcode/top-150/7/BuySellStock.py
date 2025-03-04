29b376d605c4a7c44195d5f2dc01608d8b690356
kaufon <kauanfvale@gmail.com>
2025-03-04 17:33:26 -0300

    refactor buy sell

--- leetcode/top-150/7/BuySellStock.py
+++ leetcode/top-150/7/BuySellStock.py
@@ -1,19 +1,13 @@
 class Solution(object):
     def maxProfit(self, prices):
-        maxProfits = []
-        min, maxprofit = prices[0], prices[-1]
-        for i in range(len(prices)):
-            if min > prices[i]:
-                min = prices[i]
-                maxprofit = 0
-            if maxprofit < prices[i]:
-                maxprofit = prices[i]
-            maxProfits.append(maxprofit-min)
-        maxProfit = max(maxProfits)
-        print(maxProfits)
-        if maxProfit <= 0:
-            return 0
-        return maxProfit
+        profit = 0
+        buy = prices[0]
+        for sell in prices[1:]:
+            if sell > buy:
+                profit = max(profit, sell - buy)
+            else:
+                buy = sell
+        return profit
 
 
 Solution.maxProfit(1, [3, 8, 1, 4, 7, 5])
