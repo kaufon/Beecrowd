65eaaa227825160201d18c87f4731a2d23129772
kaufon <kauanfvale@gmail.com>
2025-03-06 18:41:47 -0300

    array teo sum

---
+++ leetcode/neetcode/top-150/Array/3/TwoSum.py
@@ -0,0 +1,9 @@
+class Solution:
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
+        for index in range(len(nums)-1):
+            for j in range(index+1, len(nums)):
+                if nums[index] + nums[j] == target:
+                    return([index,j])
+
+
+Solution.twoSum(1, [5,5], 10)
