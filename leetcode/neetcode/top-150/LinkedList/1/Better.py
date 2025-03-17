126aace4bdb0415df0b5fee0725632ddc0065ba5
kaufon <kauanfvale@gmail.com>
2025-03-17 17:59:12 -0300

    understand better the linked list and the algorithm

---
+++ leetcode/neetcode/top-150/LinkedList/1/Better.py
@@ -0,0 +1,21 @@
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, curr = None, head
+        while curr:
+            nxt = curr.next
+            curr.next = prev
+            prev = curr
+            curr = nxt
+
+
+head = ListNode(0, ListNode(1, ListNode(2, None)))
+result = Solution.reverseList(1, head)
