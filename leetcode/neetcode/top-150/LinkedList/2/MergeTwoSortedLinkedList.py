03ee786fa2c9e2d900c71ccbbb2f6fe70d149e2c
kaufon <kauanfvale@gmail.com>
2025-03-17 17:28:22 -0300

    merge two linked list

---
+++ leetcode/neetcode/top-150/LinkedList/2/MergeTwoSortedLinkedList.py
@@ -0,0 +1,35 @@
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        res = tail = ListNode()
+        l1, l2 = list1, list2
+        while l1 and l2:
+            if l1.val > l2.val:
+                tail.next = l2
+                l2 = l2.next
+            else:
+                tail.next = l1
+                l1 = l1.next
+            tail = tail.next
+        return res.next
+
+
+def print_list(head: Optional[ListNode]):
+    while head:
+        print(head.val, end=" -> ")
+        head = head.next
+    print("None")
+
+
+head = ListNode(1, ListNode(2, ListNode(3)))
+head2 = ListNode(2, ListNode(3))
+merged = Solution.mergeTwoLists(1, head, head2)
+print_list(merged)
