36b83ad89d720ed7a93ee0e15cc404e95acf41d2
kaufon <kauanfvale@gmail.com>
2025-04-29 19:50:32 -0300

    fully complete exercicse bolsa

--- maratonas/internas/1-sem-2025/ExercicioE/bolsa.py
+++ maratonas/internas/1-sem-2025/ExercicioE/bolsa.py
@@ -1,15 +1,14 @@
-tecido, prevendas = map(int, input().split())
+tecido, prevenda = map(int, input().split())
 vendas = []
 for i in range(3):
-    custo, lucro = map(int, input().split())
-    if tecido / custo >= prevendas:
+    custo, lucro = (map(int, input().split()))
+    if tecido / custo >= prevenda:
         vendas.append([custo, lucro])
-if len(vendas) == 0:
+if len(vendas) <= 0:
     print("IMPOSSIVEL")
 else:
     dp = [0] * (tecido+1)
     for custo, lucro in vendas:
         for j in range(custo, tecido+1):
-            print(dp,dp[j],dp[j-custo]+lucro)
-            dp[j] = max(dp[j], dp[j-custo]+lucro)
-    # print(dp[tecido])
+            dp[j] = max(dp[j], dp[custo-tecido]+lucro)
+    print(dp[j])
